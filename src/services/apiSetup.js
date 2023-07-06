f46c42907a50d17fd7d31b3903fa1eb20b371ff3
Casuso <alberto.fernandez.c@outsourcing.cepsa.com>
2023-07-06 14:00:16 +0200

    working

--- src/services/apiSetup.js
+++ src/services/apiSetup.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const emptyApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl:"http://localhost:3000"
+    baseUrl: "",
   }),
   tagTypes: ["test"],
   endpoints: () => ({}),
